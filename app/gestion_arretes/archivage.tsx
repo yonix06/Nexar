af8e98f433b48c3a0afe8ddfc275d26312596ec1
Yonix <15114126+yonix06@users.noreply.github.com>
2024-08-05 20:38:03 +0200

    Update archivage.tsx

--- app/gestion_arretes/archivage.tsx
+++ app/gestion_arretes/archivage.tsx
@@ -4,21 +4,7 @@ import { Button } from "components/Button/Button"
 
 export const metadata: Metadata = {
   title: "Next.js archivage",
-  twitter: {
-    card: "summary_large_image",
-  },
-  openGraph: {
-    url: "https://next-enterprise.vercel.app/",
-    images: [
-      {
-        width: 1200,
-        height: 630,
-        url: "https://raw.githubusercontent.com/Blazity/next-enterprise/main/.github/assets/project-logo.png",
-      },
-    ],
-  },
 }
-
 
 export default function Archivage() {
   return (
