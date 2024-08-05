51352d6c4210fe4ba156467e46767e4b165e78e8
Yonix <15114126+yonix06@users.noreply.github.com>
2024-08-05 20:38:44 +0200

    Update diffusion.tsx

--- app/gestion_arretes/diffusion.tsx
+++ app/gestion_arretes/diffusion.tsx
@@ -5,7 +5,7 @@ import { Button } from "components/Button/Button"
 
 
 
-export default function Web() {
+export default function diffusion() {
   return (
     <>
       <section className="bg-white dark:bg-gray-900">
