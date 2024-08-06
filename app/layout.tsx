86a45e33e648138fbe0926b8d5403b1d5d8ddecb
Yonix06 <fr.may.johann@gmail.com>
2024-08-06 22:44:57 +0200

    style(app/): :construction: style

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,14 +14,18 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <title>Nexarr</title>
       </head>
       <body>
-        <header className="bg-gradient-to-b from-grey-800 to-blue-300 text-white py-4">
+        <header className="bg-gradient-to-b from-grey-700 to-blue-300 text-white py-1 w-full">
           <nav className="container mx-auto flex justify-between">
-            <h1 className="text-2xl font-bold">
+            <h1 className="text-2xl font-bold flex">
+            <Link href="https://github.com/Yonix06/">
+                <Image src="/logo/nexar.svg" width={40} height={40} alt="Nexar" className="mx-auto" />
+              </Link>
+              &nbsp;&nbsp;&nbsp;
               <Link href="/">
                 <Image src="/img/Logo-4.png" width={90} height={40} alt="Logo" className="mx-auto" />
               </Link>
             </h1>
-            <ul className="flex space-x-4 space-y-2">
+            <ul className="flex space-x-4 space-y-2 text-black font-bold text-lg">
               <li></li> {/* Non c'est pas une coquille ne pas toucher SVP */}
               <li>
                 <Link href="/">Accueil</Link>
@@ -43,7 +47,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           {children}
         </main>
 
-        <footer className="bg-blue-600 text-white py-4 fixed bottom-0 w-full">
+        <footer className="bg-blue-600 text-white py-1 fixed bottom-0 w-full">
           <div className="container mx-auto text-center">
             <p>&copy; @Yonix06 sur Github</p>
           </div>
