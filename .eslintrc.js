0778b19a40e2fa366acc4b4a15bebf9784eaf201
Y0n1x <fr.may.johann@gmail.com>
2024-08-14 16:17:31 +0200

    build(/rootdir): :fire: On refait tout au propre et en moins gros

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,92 +1,128 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const fs = require("fs")
-
 module.exports = {
-  extends: [
-    "next",
-    "prettier",
-    "react-app",
-    "react-app/jest",
-    "plugin:storybook/recommended",
-    "plugin:tailwindcss/recommended",
-    'mantine',
-    'plugin:@next/next/recommended',
-    'plugin:jest/recommended'
-  ],
-  plugins: ['testing-library', 'jest'],
+  extends: ['next/core-web-vitals', 'plugin:@typescript-eslint/recommended', 'plugin:import/recommended', 'prettier'],
+  rules: {
+    'jsx-a11y/alt-text': 'off',
+    'react/display-name': 'off',
+    'react/no-children-prop': 'off',
+    '@next/next/no-img-element': 'off',
+    '@next/next/no-page-custom-font': 'off',
+    '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    'lines-around-comment': [
+      'error',
+      {
+        beforeBlockComment: true,
+        beforeLineComment: true,
+        allowBlockStart: true,
+        allowObjectStart: true,
+        allowArrayStart: true
+      }
+    ],
+    'padding-line-between-statements': [
+      'error',
+      {
+        blankLine: 'any',
+        prev: 'export',
+        next: 'export'
+      },
+      {
+        blankLine: 'always',
+        prev: ['const', 'let', 'var'],
+        next: '*'
+      },
+      {
+        blankLine: 'any',
+        prev: ['const', 'let', 'var'],
+        next: ['const', 'let', 'var']
+      },
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: ['function', 'multiline-const', 'multiline-block-like']
+      },
+      {
+        blankLine: 'always',
+        prev: ['function', 'multiline-const', 'multiline-block-like'],
+        next: '*'
+      }
+    ],
+    'newline-before-return': 'error',
+    'import/newline-after-import': [
+      'error',
+      {
+        count: 1
+      }
+    ],
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', ['internal', 'parent', 'sibling', 'index'], ['object', 'unknown']],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before'
+          },
+          {
+            pattern: 'next/**',
+            group: 'external',
+            position: 'before'
+          },
+          {
+            pattern: '~/**',
+            group: 'external',
+            position: 'before'
+          },
+          {
+            pattern: '@/**',
+            group: 'internal'
+          }
+        ],
+        pathGroupsExcludedImportTypes: ['react', 'type'],
+        'newlines-between': 'always-and-inside-groups'
+      }
+    ],
+    '@typescript-eslint/ban-types': [
+      'error',
+      {
+        extendDefaults: true,
+        types: {
+          Function: 'Use a specific function type instead',
+          Object: 'Use object instead',
+          Boolean: 'Use boolean instead',
+          Number: 'Use number instead',
+          String: 'Use string instead',
+          Symbol: 'Use symbol instead',
+          any: false,
+          '{}': false
+        }
+      }
+    ]
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    },
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx']
+    },
+    'import/resolver': {
+      node: {},
+      typescript: {
+        project: './tsconfig.json'
+      }
+    }
+  },
   overrides: [
     {
-      files: ['**/?(*.)+(spec|test).[jt]s?(x)'],
-      extends: ['plugin:testing-library/react'],
-    },
-  ],
-  parserOptions: {
-    project: './tsconfig.json',
-    babelOptions: {
-      presets: [require.resolve("next/babel")],
-    },
-  },
-  rules: {
-    "tailwindcss/no-custom-classname": "off",
-    "testing-library/prefer-screen-queries": "off",
-    "@next/next/no-html-link-for-pages": "off",
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
-      {
-        argsIgnorePattern: "^_",
-        varsIgnorePattern: "^_",
-      },
-    ],
-    "sort-imports": [
-      "error",
-      {
-        ignoreCase: true,
-        ignoreDeclarationSort: true,
-      },
-    ],
-    "tailwindcss/classnames-order": "off",
-    'react/react-in-jsx-scope': 'off',
-    'import/extensions': 'off',
-    "import/order": [
-      1,
-      {
-        groups: ["external", "builtin", "internal", "sibling", "parent", "index"],
-        pathGroups: [
-          ...getDirectoriesToSort().map((singleDir) => ({
-            pattern: `${singleDir}/**`,
-            group: "internal",
-          })),
-          {
-            pattern: "env",
-            group: "internal",
-          },
-          {
-            pattern: "theme",
-            group: "internal",
-          },
-          {
-            pattern: "public/**",
-            group: "internal",
-            position: "after",
-          },
-        ],
-        pathGroupsExcludedImportTypes: ["internal"],
-        alphabetize: {
-          order: "asc",
-          caseInsensitive: true,
-        },
-      },
-    ],
-  },
-}
-
-function getDirectoriesToSort() {
-  const ignoredSortingDirectories = [".git", ".next", ".vscode", "node_modules"]
-  return getDirectories(process.cwd()).filter((f) => !ignoredSortingDirectories.includes(f))
-}
-
-function getDirectories(path) {
-  return fs.readdirSync(path).filter(function (file) {
-    return fs.statSync(path + "/" + file).isDirectory()
-  })
+      files: ['*.ts', '*.tsx', 'src/iconify-bundle/*'],
+      rules: {
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-var-requires': 'off'
+      }
+    }
+  ]
 }
---
+++ src/app/authentication/login/page.tsx
@@ -0,0 +1,96 @@
+"use client";
+import Link from "next/link";
+import { Grid, Box, Card, Stack, Typography } from "@mui/material";
+// components
+import PageContainer from "components/container/PageContainer";
+import Logo from "../../layout/shared/logo/Logo";
+import AuthLogin from "../auth/AuthLogin";
+
+const Login2 = () => {
+  return (
+    <PageContainer title="Nexar Accueil" description="Bienvenue">
+      <Box
+        sx={{
+          position: "relative",
+          "&:before": {
+            content: '""',
+            background: "radial-gradient(#d2f1df, #d3d7fa, #bad8f4)",
+            backgroundSize: "400% 400%",
+            animation: "gradient 15s ease infinite",
+            position: "absolute",
+            height: "100%",
+            width: "100%",
+            opacity: "0.3",
+          },
+        }}
+      >
+        <Grid
+          container
+          spacing={0}
+          justifyContent="center"
+          sx={{ height: "100vh" }}
+        >
+          <Grid
+            item
+            xs={12}
+            sm={12}
+            lg={4}
+            xl={3}
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Card
+              elevation={9}
+              sx={{ p: 4, zIndex: 1, width: "100%", maxWidth: "500px" }}
+            >
+              <Box display="flex" alignItems="center" justifyContent="center">
+                <Logo />
+              </Box>
+              <AuthLogin
+                subtext={
+                  <Typography
+                    variant="subtitle1"
+                    textAlign="center"
+                    color="textSecondary"
+                    mb={1}
+                  >
+                    Gestion des arrêtés municipaux
+                  </Typography>
+                }
+                subtitle={
+                  <Stack
+                    direction="row"
+                    spacing={1}
+                    justifyContent="center"
+                    mt={3}
+                  >
+                    <Typography
+                      color="textSecondary"
+                      variant="h6"
+                      fontWeight="500"
+                    >
+                      Nouveau sur Nexar ?
+                    </Typography>
+                    <Typography
+                      component={Link}
+                      href="/authentication/register"
+                      fontWeight="500"
+                      sx={{
+                        textDecoration: "none",
+                        color: "primary.main",
+                      }}
+                    >
+                      On s'enregistre avec son mail professionnel ici
+                    </Typography>
+                  </Stack>
+                }
+              />
+            </Card>
+          </Grid>
+        </Grid>
+      </Box>
+    </PageContainer>
+  );
+};
+export default Login2;
