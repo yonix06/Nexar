6bff0662a38ebb5099f1e4d71c421a7b062473fb
Y0n1x <fr.may.johann@gmail.com>
2024-08-18 16:38:06 +0200

    .

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,28 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
-  stories: ["../**/lib/**/*.@(mdx|stories.@(js|jsx|ts|tsx))"],
-  addons: ["@storybook/addon-essentials", "@storybook/addon-interactions"],
+  stories: [
+    "../components/**/**/*.@(mdx|stories.@(js|jsx|ts|tsx))",
+    "../components/**/*.@(mdx|stories.@(js|jsx|ts|tsx))",
+    "../components/*.@(mdx|stories.@(js|jsx|ts|tsx))"
+  ],
+
+  addons: [
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@chromatic-com/storybook"
+  ],
+
   framework: {
     name: "@storybook/nextjs",
     options: {},
   },
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript"
+  }
 };
 
 export default config;
