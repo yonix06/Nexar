02855a4ee5e286f0b589132ee228fc69c5c2d451
Y0n1x <fr.may.johann@gmail.com>
2024-08-18 21:19:13 +0200

    fix(extensions): :chart_with_upwards_trend:

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,13 +4,12 @@ import { sfPro, inter } from "./fonts";
 import Nav from "@/components/layout/nav";
 import Footer from "@/components/layout/footer";
 import { Suspense } from "react";
-import { Analytics as VercelAnalytics } from "@vercel/analytics/react";
 
 export const metadata = {
-  title: "Precedent - Building blocks for your Next.js project",
+  title: "Nexar - Application de gestion des arrêtés municipaux",
   description:
-    "Precedent is the all-in-one solution for your Next.js project. It includes a design system, authentication, analytics, and more.",
-  metadataBase: new URL("https://precedent.dev"),
+    "Solution de création, diffusion, cartographie et suivi des arrêtés municipaux",
+  metadataBase: new URL("https://localhost:3000/"),
 };
 
 export default async function RootLayout({
@@ -19,17 +18,16 @@ export default async function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="fr">
       <body className={cx(sfPro.variable, inter.variable)}>
-        <div className="fixed h-screen w-full bg-gradient-to-br from-indigo-50 via-white to-cyan-100" />
+        <div className="fixed w-full h-screen bg-gradient-to-br from-indigo-50 via-white to-cyan-100" />
         <Suspense fallback="...">
           <Nav />
         </Suspense>
-        <main className="flex min-h-screen w-full flex-col items-center justify-center py-32">
+        <main className="flex flex-col items-center justify-center w-full min-h-screen py-32">
           {children}
         </main>
         <Footer />
-        <VercelAnalytics />
       </body>
     </html>
   );
