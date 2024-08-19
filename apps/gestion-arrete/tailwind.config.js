71a4cf8bf39a56e7ca748fc2b4b28f56d81072de
Y0n1x <fr.may.johann@gmail.com>
2024-08-19 18:54:18 +0200

    modif pour appli 1

--- apps/gestion-arrete/tailwind.config.js
+++ apps/gestion-arrete/tailwind.config.js
@@ -1,6 +1,7 @@
 const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
 const { join } = require('path');
 
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
