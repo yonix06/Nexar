657423f8248e944a8e804ce70b3dcc803e881a5f
Y0n1x <fr.may.johann@gmail.com>
2024-08-21 23:27:33 +0200

    build(Config globale (nx.json | tsconfig.base.json)): :bug: @tailwindcss/typography + "composite": true,

---
+++ apps/portal/src/server/api/trpc.controller.ts
@@ -0,0 +1,35 @@
+import { TRPCError } from '@trpc/server';
+import { z, ZodError } from 'zod';
+
+import {
+	createTRPCRouter,
+	// protectedProcedure,
+	publicProcedure,
+} from '../server/api/trpc';
+
+export const getAll = async () => {
+	try {
+		// ... your code goes here
+	} catch (error) {
+		if (error instanceof ZodError) {
+			throw new TRPCError({
+				code: 'BAD_REQUEST',
+				message: error.message,
+			});
+		}
+
+		if (error instanceof TRPCError) {
+			if (error.code === 'UNAUTHORIZED') {
+				throw new TRPCError({
+					code: 'UNAUTHORIZED',
+					message: 'Unauthorized',
+				});
+			}
+
+			throw new TRPCError({
+				code: 'INTERNAL_SERVER_ERROR',
+				message: error.message,
+			});
+		}
+	}
+};
