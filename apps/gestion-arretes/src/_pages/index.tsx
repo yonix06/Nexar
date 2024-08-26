1ffb33b41ef6df231a82c1cd234bb7f78b715ca1
Y0n1x <fr.may.johann@gmail.com>
2024-08-27 00:34:44 +0200

    bye pages

--- apps/gestion-arretes/src/_pages/index.tsx
+++ apps/gestion-arretes/src/_pages/index.tsx
@@ -1,6 +1,6 @@
 import { GetStaticProps } from 'next';
 import { countComponentsByCategory, getAllComponents } from '../data/components';
-import { HomePage } from '../components/HomePage/HomePage';
+import { HomePage } from '../_components/HomePage/HomePage';
 
 export default HomePage;
 
